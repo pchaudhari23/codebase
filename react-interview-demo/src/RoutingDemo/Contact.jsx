340e8aa122ab500a186f5cb32c37ef76fa5f9539
Pritam <pritam2310@gmail.com>
2025-01-11 19:13:23 +0530

    Removed Javascript stuff

--- react-interview-demo/src/RoutingDemo/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
-const Contact = () => {
-    let { contactID } = useParams();
-    return <h3>Selected contact: {contactID}</h3>
-}
-
-export default Contact
--- react-interview-demo/src/RoutingDemo/Documentation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Documentation = () => {
-  return (
-    <h1>Documentation Page</h1>
-  )
-}
-
-export default Documentation
